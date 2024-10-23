0b35b163c503760dabe4558b1690a4591e0fe3cb
VoidAutomata <82106210+VoidAutomata@users.noreply.github.com>
2024-10-22 21:55:45 -0400

    All Commands Added
    Commands and printf lines functional.
    
    To do: 
    - Start_Alarm needs to properly insert new alarm into alarm_list. Logic is mostly written
    - Other threads such as alarm and display threads

--- New_alarm_mutex.c
+++ New_alarm_mutex.c
@@ -27,8 +27,9 @@
  */
 typedef struct alarm_tag {
     struct alarm_tag    *link; //link to next alarm in list
-    int                 alarm_id; //ADDED By Chris: unique alarm ID to identify and edit
-    int                 seconds;
+    int                 alarm_id; // unique alarm ID to identify and edit
+    int 				seconds; // will use this to measure time
+    char* 				type; // type in string format, does not include T at start
     time_t              time;   /* seconds from EPOCH */
     char                message[64];
 } alarm_t;
@@ -111,6 +112,7 @@ void *alarm_thread (void *arg)
 //____ FUNCTIONS ____
 
 // Function to trim leading and trailing whitespace
+// Code provided by Adam Rosenfield and Dave Gray on StackOverflow
 char *trimwhitespace(char *str)
 {
   char *end;
@@ -142,39 +144,184 @@ void Start_Alarm (int alarm_id, char* type, int seconds, const char* message){
     // MUST FREE MEMORY ONCE ALARM EXPIRES
     alarm = (alarm_t*)malloc(sizeof(alarm_t));
     if (alarm == NULL){
+		free (alarm);
         perror("Allocate Alarm");
-        free (alarm);
     }
-    free(alarm);
+    alarm->alarm_id = alarm_id;
+    alarm->type = type;
+    alarm->seconds = seconds;
+    alarm->time = time(NULL) + seconds; // current time + seconds
+    strcpy(alarm->message, message); // copy string into the struct
+    alarm->link = NULL;
+
+    // lock mutex before operation
+    status = pthread_mutex_lock(&alarm_mutex);
+    if (status != 0){
+        err_abort (status, "Lock Mutex");
+    }
+    
+    last = &alarm_list;
+    next = *last;
+
+    while(next!= NULL){
+        // compare items in list until current alarm_id is greater than last
+        if (next->alarm_id >= alarm_id){
+            alarm->link = next; //position found. Link alarm
+            break;
+        }
+        last = &next->link; // move to next position in list
+        next = next->link;
+    }
+    alarm_list = alarm; // insert new alarm at found position
+
+    //printf("Successfully added alarm %d to list.\n", alarm->alarm_id);
+
+    // unlock mutex after operation
+    status = pthread_mutex_unlock(&alarm_mutex);
+    if (status != 0){
+        err_abort (status, "Unlock Mutex");
+    }
+    
 }
 void Change_Alarm (int alarm_id, char* type, int seconds, const char* message){
-		printf("Changing alarm %d to %s, %d, %s\n", alarm_id, type, seconds, message);
+        alarm_t *alarm; // pointer for new alarm
+        int status; // for checking mutex status
+		printf("Changing alarm %d to T%s, %d, %s\n", alarm_id, type, seconds, message);
+
+        // lock mutex
+        status = pthread_mutex_lock(&alarm_mutex);
+        if (status != 0){
+            err_abort (status, "Lock Mutex");
+        }
+
+        // search for matching alarm based on alarm_id
+        alarm = alarm_list;
+        while (alarm != NULL){
+            if (alarm->alarm_id == alarm_id){
+                // found the alarm. Update fields
+                strcpy(alarm->type, type);
+                alarm->type[sizeof(alarm->type)-1] = '\0'; // set last value as terminator
+                alarm->seconds = seconds;
+                alarm->time = time(NULL) + seconds; // based on current time
+                strcpy(alarm->message, message);
+                alarm->message[sizeof(alarm->message)-1] = '\0'; // terminate string
+
+                //printf("Alarm %d has been changed to T%s %d %s.\n", alarm_id, type, seconds, message);
+                break;
+            }
+            // didn't find. move to next in list
+            alarm = alarm->link;
+        }
+
+        if (alarm == NULL){
+            // this alarm_id doesn't exist in the list
+            printf("Could not find alarm %d\n", alarm_id);
+        }
+
+        // unlock mutex
+        status = pthread_mutex_unlock(&alarm_mutex);
+        if (status != 0){
+            err_abort (status, "Unlock Mutex");
+        }
 	}
 
 void Cancel_Alarm (int alarm_id){
+    alarm_t *alarm, *prev;
+    int status;
+
     printf("Canceling alarm %d\n", alarm_id);
+    //free(alarm);
+
+    // lock mutex
+    status = pthread_mutex_lock(&alarm_mutex);
+    if (status != 0){
+        err_abort (status, "Lock Mutex");
+    }
+
+    // search for alarm in alarm_list to cancel
+    prev = NULL;
+    alarm = alarm_list;
+
+    while (alarm != NULL){
+        if (alarm->alarm_id == alarm_id){
+            // found alarm. remove from list
+            if (prev == NULL){
+                // alarm is the first in list. make link the new head
+                alarm_list = alarm->link;
+            } else {
+                prev->link = alarm->link;
+            }
+            // free memory
+            free(alarm);
+            break;
+        }
+        // move to next alarm in list
+        prev = alarm;
+        alarm = alarm->link;
+    }
+
+    if (alarm == NULL){
+        // alarm to find does not exist
+        printf("Alarm %d does not exist.\n", alarm_id);
+    }
+
+    // unlock mutex
+    status = pthread_mutex_unlock(&alarm_mutex);
+    if (status != 0){
+        err_abort (status, "Unlock Mutex");
+    }    
 }
 
 void View_Alarms(){
+    alarm_t *alarm;
+    int status;
+    time_t now;
+    int time_left;
+
     printf("Viewing Alarms\n");
+    
+    // lock mutex
+    status = pthread_mutex_lock(&alarm_mutex);
+    if (status != 0){
+        err_abort (status, "Lock Mutex");
+    }
+
+    // get current time. needed when time changes
+    now = time(NULL);
+
+    // check alarm list
+    if (alarm_list == NULL){
+        printf("There are no alarms.\n");
+    } else {
+        // go through each alarm in list
+        for (alarm = alarm_list; alarm != NULL; alarm = alarm->link){
+            time_left = (int)(alarm->time - now);
+            printf("Alarm(%d): T%s %d time left: %d seconds. Message: %s",
+            alarm->alarm_id, alarm->type, alarm->seconds, time_left, alarm->message);
+        }
+    }
+
+    // unlock mutex
+    status = pthread_mutex_unlock(&alarm_mutex);
+    if (status != 0){
+        err_abort (status, "Unlock Mutex");
+    }
 }
 
+// Below is the main function/thread
 int main (int argc, char *argv[])
 {
     int status;
     char sline[128];
     char line[128];
     alarm_t *alarm, **last, *next;
-    int alarm_id; // ADDED: declare the alarm's unique id
-  //  char* type; // ADDED: type string. Usually in form 'T2'
+    int alarm_id; // declare the alarm's unique id
     char type[65] = "";
-    int seconds; // ADDED: time in seconds
-  //  char* message; // ADDED: message of alarm when expired
+    int seconds; // time in seconds
     char message[65] = "";
     pthread_t thread;
 
-    status = pthread_create (
-        &thread, NULL, alarm_thread, NULL);
+    status = pthread_create (&thread, NULL, alarm_thread, NULL);
     if (status != 0)
         err_abort (status, "Create alarm thread");
     while (1) {
@@ -185,27 +332,34 @@ int main (int argc, char *argv[])
         if (alarm == NULL)
             errno_abort ("Allocate alarm");
 
-        // ADDED: Check command for specific function calls {
-        // Start Alarm function call
+        // remove unnecessary white space around line (stdin)
         strcpy(line, trimwhitespace(sline));
         
-            
-        if (sscanf(line, "Start_Alarm(%d): %s %d %s", 
+        // check which function was called
+        if (sscanf(line, "Start_Alarm(%d): T%s %d %128[^\n]", 
             &alarm_id, type, &seconds, message) > 3){
-                Start_Alarm(alarm_id, type, seconds, message);
+            Start_Alarm(alarm_id, type, seconds, message);
+            printf("Alarm(%d) Inserted by Main Thread(<thread-id>) Into Alarm List at %d: T%s %d %s",
+            alarm_id, (int)time(NULL), type, seconds, message);
+
             // Change Alarm function call
-        } else if (sscanf(line, "Change_Alarm(%d): %s %d %s", 
+        } else if (sscanf(line, "Change_Alarm(%d): T%s %d %128[^\n]", 
             &alarm_id, type, &seconds, message) > 3){
-                Change_Alarm(alarm_id, type, seconds, message);
+            Change_Alarm(alarm_id, type, seconds, message);
+            printf("Alarm(%d) Changed at %d: T%s %d %s",
+            alarm_id, (int)time(NULL), type, seconds, message);
+
             // Cancel Alarm function call
         } else if (sscanf(line, "Cancel_Alarm(%d)", 
             &alarm_id) == 1){
-                Cancel_Alarm(alarm_id);
+            Cancel_Alarm(alarm_id);
+            printf("Alarm(%d) Cancelled at %d: T%s %d %s",
+            alarm_id, (int)time(NULL), type, seconds, message);
+
             // View Alarms function call. Uses specifically string compare, not sscanf
             // There are no variables to compare, only exact copy of a string
-        } else if (strcmp(line, "View_Alarms()\n") == 0){
+        } else if (strcmp(line, "View_Alarms()") == 0){
                 View_Alarms();
-        // }
         /*
          * Parse input line into seconds (%d) and a message
          * (%64[^\n]), consisting of up to 64 characters
